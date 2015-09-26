43d26f43d7fb81ec7c9a616f4d52a77366c6f5c1
RyuUramoto <dutycatsjam@gmail.com>
2015-09-26 13:40:07 +0900

    shopのseedにサンプルデータを作成．

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@shop = Shop.new
+@shop[:name] = "pastadeka"
+@shop[:image_path] = ""
+@shop[:address] = "福岡県福岡市"
+@shop[:tel] = "0120-123-456"
+@shop[:abstract] = "概要"
+@shop[:store_hours] = "12:00 ~ 18:00"
+@shop[:category] = "food, ring"
+@shop[:situation] = "present"
+@shop.save
+
+@shop = Shop.new
+@shop[:name] = "spicaterible"
+@shop[:image_path] = "spica"
+@shop[:address] = "福岡県福岡市"
+@shop[:tel] = "0120-888-888"
+@shop[:abstract] = "スピカ概要"
+@shop[:store_hours] = "10:00 ~ 20:00"
+@shop[:category] = "ring, earrings, sunglasses"
+@shop[:situation] = "present, birthday"
+@shop.save
+
+@shop = Shop.new
+@shop[:name] = "パックマン"
+@shop[:image_path] = ""
+@shop[:address] = "福岡県宗像市"
+@shop[:tel] = "0120-4444-4444"
+@shop[:abstract] = "概要パックマン"
+@shop[:store_hours] = "12:00 ~ 24:00"
+@shop[:category] = "accessory, necklace, ring"
+@shop[:situation] = "birthday"
+@shop.save
+
+@shop = Shop.new
+@shop[:name] = "マ・マー・マー"
+@shop[:image_path] = ""
+@shop[:address] = "福岡県和白"
+@shop[:tel] = "0880-5296-1111"
+@shop[:abstract] = "概要パスタ"
+@shop[:store_hours] = "6:00 ~ 18:00"
+@shop[:category] = "food, ring, necklace"
+@shop[:situation] = "wedding, birthday"
+@shop.save
+
+@shop = Shop.new
+@shop[:name] = "パラサイト・イヴ二ング"
+@shop[:image_path] = ""
+@shop[:address] = "福岡県和白"
+@shop[:tel] = "0120-202-2222"
+@shop[:abstract] = "概要触手"
+@shop[:store_hours] = "20:00 ~ 3:00"
+@shop[:category] = "food, watch, earrings"
+@shop[:situation] = "present"
+@shop.save
